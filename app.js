9c42f79ce661074a6c1a8653430b4781f6b9ea32
mf <myemail@isawesome.com>
2017-04-20 15:14:24 -0500

    added ties compare

--- app.js
+++ app.js
@@ -14,19 +14,14 @@ var submits = 0;
 var rocks = [];
 var papers = [];
 var scissors = [];
-// var allArrays = [];
-// allArrays.push(rocks);
-// allArrays.push(papers);
-// allArrays.push(scissors);
+
 
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket) {
     console.log('connection made!');
-    // Example communication.
-    socket.emit('message', 'We are connected!');
-// Broadcast isn't working like this, but doesn't crash anything
-    socket.broadcast.emit('message2', 'I guess we have a newcomer');
+
+
 
     socket.on('setUsername', function(username) {
         // Creates object variable to hold username.
@@ -34,7 +29,10 @@ io.sockets.on('connection', function(socket) {
         socket.username = username;
         players.push(socket);
         console.log(socket.username + " added");
+        socket.broadcast.emit('message', 'I guess we have a newcomer, ' + socket.username);
     });
+// Broadcast isn't working like this, but doesn't crash anything
+
 
     socket.on('selectedWeapon', function(choice) {
         socket.weapon = choice;
@@ -55,43 +53,13 @@ io.sockets.on('connection', function(socket) {
 
         if (submits == players.length) {
 
-            // if (rocks.length > 1) { }
 
             if (rocks.length > 0 && papers.length > 0) { emitWins(papers, rocks); }
             if (rocks.length > 0 && scissors.length > 0) { emitWins(rocks, scissors); }
             if (papers.length > 0 && scissors.length > 0) { emitWins(scissors, papers); }
-
-
-
-
-            // for (var x = 0; x < players.length; x++) {
-            //     for (var y = 0; y < players.length; y++) {
-            //         if (players[x] != players[y]) {
-            //             if (players[x].weapon == players[y].weapon) {
-            //                 console.log("tie");
-            //                 break;
-            //             }
-            //             else if (players[x].weapon == "rock") {
-            //                 if (players[y].weapon == "paper") {
-            //                     console.log("Player " + players[y].username + " wins");
-            //                 } else {
-            //                     console.log("Player " + players[x].username + " wins");
-            //                 }
-            //             }
-            //             else if (players[x].weapon == "paper") {
-            //                 if (players[y].weapon == "scissors") {
-            //                     console.log("Player " + players[x].username + " wins");
-            //                 } else {
-            //                     console.log("Player " + players[y].username + " wins");
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }
-
-
-
-
+            if (rocks.length > 1) { emitTies(rocks); }
+            if (papers.length > 1) { emitTies(papers); }
+            if (scissors.length > 1) { emitTies(scissors); }
 
             submits = 0;
             rocks.length = 0;
@@ -110,62 +78,16 @@ function emitWins(winners, losers) {
     }
 }
 
+function emitTies(tiers) {
+    var tiersStr = "";
+    for (var x = 0; x < tiers.length - 1; x++) {
+        tiersStr += tiers[x].username + ", ";
+    }
+    tiersStr += tiers[tiers.length -1].username;
+    io.sockets.emit('outcome', tiersStr + " Tied");
+}
+
 server.listen(3000);
-
-
-
-
-// var express = require('express');
-// var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
-//
-// var index = require('./routes/index');
-// var users = require('./routes/users');
-//
-// var app = express();
-//
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
-//
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// app.use('/', index);
-// app.use('/users', users);
-//
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-//
-// module.exports = app;
-
-
-
-
-
 
 // MOST helpful site I've come across! (structure above adopted from site):
 // https://openclassrooms.com/courses/ultra-fast-applications-using-node-js/socket-io-let-s-go-to-real-time
