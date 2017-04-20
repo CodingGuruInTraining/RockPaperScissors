7349c816dad6feffd2aff012984311e07edcc2a6
mf <myemail@isawesome.com>
2017-04-20 16:41:59 -0500

    added comments

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
 var http = require('http');
 var fs = require('fs');
 
+// Creates server and sends client page.
 var server = http.createServer(function(req, res) {
     fs.readFile('./index.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
     });
-
-    // fs.readFile('./public/stylesheets/style.css', 'utf-8', function(error, content) {
-    //     res.writeHead(200, {"Content-Type": "text/css"});
-    //     res.end(content);
-    // });
 });
 
+// Empty array to hold current players.
 var players = [];
+// Counter for players who selected weapon.
 var submits = 0;
 
+// Empty arrays to hold players based on their weapon.
 var rocks = [];
 var papers = [];
 var scissors = [];
 
-
 var io = require('socket.io').listen(server);
 
+// Defines actions to take once websocket connects.
 io.sockets.on('connection', function(socket) {
     console.log('connection made!');
 
-
-
+    // Sets player attribute and sends message to other players.
     socket.on('setUsername', function(username) {
         // Creates object variable to hold username.
         // TODO figure out how cookies could be used instead:
         socket.username = username;
         players.push(socket);
-        console.log(socket.username + " added");
         socket.broadcast.emit('message', socket.username + ' has joined the game.');
     });
-// Broadcast isn't working like this, but doesn't crash anything
 
-
+    // Actions for selecting weapon.
     socket.on('selectedWeapon', function(choice) {
         socket.weapon = choice;
-        console.log(socket.username + " chose " + choice);
         submits++;
-
+        // Assigns player to appropriate array.
         switch (choice) {
             case "ROCK":
                 rocks.push(socket);
@@ -55,10 +50,10 @@ io.sockets.on('connection', function(socket) {
                 scissors.push(socket);
                 break;
         }
-
+        // Checks if all players have made a selection before
+        // comparing.
         if (submits == players.length) {
-
-
+            // Runs each array through a compare function.
             if (rocks.length > 0 && papers.length > 0) { emitWins(papers, rocks); }
             if (rocks.length > 0 && scissors.length > 0) { emitWins(rocks, scissors); }
             if (papers.length > 0 && scissors.length > 0) { emitWins(scissors, papers); }
@@ -66,6 +61,7 @@ io.sockets.on('connection', function(socket) {
             if (papers.length > 1) { emitTies(papers); }
             if (scissors.length > 1) { emitTies(scissors); }
 
+            // Clears objects for next game.
             submits = 0;
             rocks.length = 0;
             papers.length = 0;
@@ -75,6 +71,7 @@ io.sockets.on('connection', function(socket) {
 });
 
 function emitWins(winners, losers) {
+    // Loops through both arrays and transmits message to all players.
     for (var x = 0; x < winners.length; x++) {
         for (var y = 0; y < losers.length; y++) {
             io.sockets.emit('outcome', winners[x].username + " beats " + losers[y].username);
@@ -84,6 +81,8 @@ function emitWins(winners, losers) {
 }
 
 function emitTies(tiers) {
+    // Loops through array and creates string of players' usernames who
+    // tied. The message is then transmitted to all players.
     var tiersStr = "";
     for (var x = 0; x < tiers.length - 1; x++) {
         tiersStr += tiers[x].username + ", ";
